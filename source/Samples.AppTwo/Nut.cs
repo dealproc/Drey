17537f748f3cafc5ae4dfeb49ec3c7ac15db91e5
Richard Bennett <rbennett@academyone.com>
2015-08-10 13:00:16 -0400

    Second demo app sample, just to make sure.

---
+++ source/Samples.AppTwo/Nut.cs
@@ -0,0 +1,19 @@
+﻿using Drey.Nut;
+using System;
+
+[assembly: Drey.Nut.Cracking(typeof(Samples.AppTwo.Nut), true, "Samples - App Two")]
+namespace Samples.AppTwo
+{
+    public class Nut : IDisposable
+    {
+        public void Configuration(INutConfiguration configurationManager)
+        {
+            Console.WriteLine("Samples - App Two Online");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Samples - App Two Torn Down");
+        }
+    }
+}
