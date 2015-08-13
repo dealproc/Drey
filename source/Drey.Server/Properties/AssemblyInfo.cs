f65dde0524e9003b0ee668494d26e50a6bd0a596
Richard Bennett <rbennett@academyone.com>
2015-08-13 10:54:32 -0400

    Added nuspec for drey.server

--- source/Drey.Server/Properties/AssemblyInfo.cs
+++ source/Drey.Server/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Drey.Server")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Drey - Server APIs used to distribute packages to clients")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AcademyOne, Inc")]
 [assembly: AssemblyProduct("Drey.Server")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
