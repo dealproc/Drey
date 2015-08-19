18ddc40e2dcd902cab142953f95f3322411e0f5a
Richard Bennett <rbennett@academyone.com>
2015-08-19 19:36:24 -0400

    Nuget.exe compatible endpoint.  Accepts pushes from  instead of the need for a custom client tool.

---
+++ source/Drey.Server/Modules/well-known/NugetUploadModule.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Drey.Server.Modules.well_known
+{
+    public class NugetUploadModule : NancyModule
+    {
+        const string NUGET_DIR = @"c:\packages";
+
+        public NugetUploadModule() : base("/api/v2/package")
+        {
+            Put["/", runAsync: true] = ParseAndStorePackage;
+        }
+
+        private async Task<dynamic> ParseAndStorePackage(dynamic _, CancellationToken ct)
+        {
+            if (!Request.Files.Any())
+            {
+                return ((Response)"Missing file").StatusCode = HttpStatusCode.BadRequest;
+            }
+
+            NuGet.ZipPackage pkg = new NuGet.ZipPackage(Request.Files.First().Value);
+
+            if (!Directory.Exists(NUGET_DIR)) { Directory.CreateDirectory(NUGET_DIR); }
+
+            var fileName = Path.Combine(NUGET_DIR, string.Format("{0}-{1}.nupkg", pkg.Id, pkg.Version));
+
+            if (File.Exists(fileName)) { File.Delete(fileName); }
+
+            using (var pkgStore = File.OpenWrite(fileName))
+            {
+                await pkg.GetStream().CopyToAsync(pkgStore);
+            }
+
+            return HttpStatusCode.Created;
+        }
+    }
+}
