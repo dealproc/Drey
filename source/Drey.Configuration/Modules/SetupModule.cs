f86f9b11a8ddb9c1a42cb1a9eda3a5f67712e828
Richard Bennett <rbennett@academyone.com>
2015-08-25 21:20:21 -0400

    Quick and dirty method to update client certificate(s) and server host name applets will be dependent on.

--- source/Drey.Configuration/Modules/SetupModule.cs
+++ source/Drey.Configuration/Modules/SetupModule.cs
@@ -1,8 +1,11 @@
 ﻿using Nancy;
 using Nancy.ModelBinding;
 using Nancy.Validation;
+
+using System;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Drey.Configuration.Modules
 {
@@ -10,12 +13,19 @@ namespace Drey.Configuration.Modules
     {
         readonly Services.IGlobalSettingsService _globalSettingsService;
 
-        public SetupModule(Services.IGlobalSettingsService globalSettingsService) : base("/Setup")
+        public SetupModule(Services.IGlobalSettingsService globalSettingsService)
+            : base("/Setup")
         {
             _globalSettingsService = globalSettingsService;
 
             Get["/"] = GetIndex;
             Post["/"] = CommitSettings;
+
+            Get["/ServerUrl"] = UpdateServerUrl;
+            Post["/ServerUrl"] = SaveNewServerUrl;
+
+            Get["/ClientCertificate"] = UpdateClientCertificate;
+            Post["/ClientCertificate"] = SaveNewClientCertificate;
         }
 
         private dynamic GetIndex(dynamic arg)
@@ -46,6 +56,60 @@ namespace Drey.Configuration.Modules
             }
 
             return View["index", settingsPmo];
+        }
+
+        private dynamic UpdateClientCertificate(dynamic arg)
+        {
+            return Negotiate.WithView("ClientCertificate");
+        }
+
+        private dynamic SaveNewClientCertificate(dynamic arg)
+        {
+            if (!Request.Files.Any())
+            {
+                ModelValidationResult.Errors.Add(string.Empty, "File did not upload.");
+            }
+
+            var certFile = Request.Files.First();
+            try
+            {
+                byte[] cert;
+                using (var ms = new MemoryStream())
+                {
+                    certFile.Value.CopyTo(ms);
+                    cert = ms.ToArray();
+                }
+
+                var clientSslObj = new X509Certificate2(cert, (string)null);
+                _globalSettingsService.UpdateSSLCertificate(cert);
+
+                return Response.AsRedirect("~/");
+            }
+            catch (Exception ex)
+            {
+                ModelValidationResult.Errors.Add(string.Empty, ex.Message);
+            }
+
+            return Negotiate.WithView("ClientCertificate");
+        }
+
+        private dynamic UpdateServerUrl(dynamic arg)
+        {
+            return Negotiate.WithView("ServerUrl").WithModel(new Services.ViewModels.ServerHostnamePmo { CurrentHostname = _globalSettingsService.GetServerHostname() });
+        }
+
+        private dynamic SaveNewServerUrl(dynamic arg)
+        {
+            var model = this.BindAndValidate<Services.ViewModels.ServerHostnamePmo>();
+
+            if (ModelValidationResult.IsValid)
+            {
+                _globalSettingsService.UpdateServerHostname(model.NewHostname);
+                return Response.AsRedirect("~/");
+            }
+
+            model.CurrentHostname = _globalSettingsService.GetServerHostname();
+            return Negotiate.WithView("ServerUrl").WithModel(model);
         }
     }
 }
---
+++ source/Drey.Configuration/Services/ViewModels/ServerHostnamePmo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Drey.Configuration.Services.ViewModels
+{
+    public class ServerHostnamePmo
+    {
+        public string CurrentHostname { get; set; }
+        
+        [Required]
+        public string NewHostname { get; set; }
+    }
+}
